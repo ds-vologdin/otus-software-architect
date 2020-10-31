8e0930ec76e52ea35042974ce1f091505d09d6d0
Dmitry Vologdin <ds.vologdin@yandex.ru>
2020-10-31 14:42:26 +0300

    fake users

---
+++ task02/migrations/0002_fake_user.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "user" (username, first_name, last_name, email, phone) VALUES
+    ('tom', 'tom', 'brown', 'tom@brown.com', '+79990002351'),
+    ('bob', 'bob', 'brown', 'bob@brown.com', '+79990002352');
